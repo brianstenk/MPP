9267e17236e72fa8e889c6514ae0728afa49c59f
Brian Nkurunungi <nkurunungib@gmail.com>
2020-05-16 02:29:02 -0500

    getHiredate

--- EmployeeInfo/src/Employee.java
+++ EmployeeInfo/src/Employee.java
@@ -1,11 +1,11 @@
 //import java.util.*;
 public class Employee extends Person {
-	//private String hireDate;
+	 private String hireDate;
 	 private double salary;
 	 
 	 public Employee(String name, int age, String hireDate, double salary) {
 		super(name, age);
-		//this.hireDate = hireDate;
+		this.hireDate = hireDate;
 		this.salary =  salary;
 		// TODO Auto-generated constructor stub
 	}
@@ -14,7 +14,7 @@ public class Employee extends Person {
 		Employee emp = new Employee("Brian", 46, "2019-09-09", 10000.0);
 		emp.setName("Emile");
 		System.out.println(emp.getName() +" earns "+emp.salary);
-		
+		System.out.println("The Employee was hired on "+emp.getHiredate());
 	}
 	 
 }
