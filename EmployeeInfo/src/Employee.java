bf7b8ae2fcee66be3782460c5af78f76be4385d5
brianstenk <nkurunungib@gmail.com>
2020-05-16 02:26:20 -0500

    Inheritance with SuperKeyword

---
+++ EmployeeInfo/src/Employee.java
@@ -0,0 +1,20 @@
+//import java.util.*;
+public class Employee extends Person {
+	//private String hireDate;
+	 private double salary;
+	 
+	 public Employee(String name, int age, String hireDate, double salary) {
+		super(name, age);
+		//this.hireDate = hireDate;
+		this.salary =  salary;
+		// TODO Auto-generated constructor stub
+	}
+
+	 public static void main(String[] args) {
+		Employee emp = new Employee("Brian", 46, "2019-09-09", 10000.0);
+		emp.setName("Emile");
+		System.out.println(emp.getName() +" earns "+emp.salary);
+		
+	}
+	 
+}
